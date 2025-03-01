44be5997161a916caecf25449f4a951911984a5d
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-01 09:15:47 +0900

    updated portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -6,5 +6,51 @@ export const metadata: Metadata = {
 };
 
 export default function PortfolioPage() {
-  return <div>PortfolioPage</div>;
+  return (
+    <div className="max-w-5xl mx-auto px-4">
+      <div className="border border-muted-foreground rounded-xl">
+        <video
+          src="/wearly.mp4"
+          muted
+          loop
+          autoPlay={true}
+          className="rounded-t-xl"
+        />
+        <p className="flex justify-center mt-4 text-xl font-mono font-bold">
+          <a
+            href="http://www.wearly.vercel.app"
+            target="_blank"
+            className="hover:underline-offset-4 hover:underline animate-pulse duration-30000"
+          >
+            Wearly.
+          </a>
+        </p>
+        <p className="text-muted-foreground text-center mt-2 font-mono lg:px-12 pb-4 text-sm px-4">
+          A modern e-commerce website using{" "}
+          <span className="text-lime-300 font-bold">Next.js</span> for fast{" "}
+          <span className="text-lime-300 font-bold">
+            NextAuth for authentication
+          </span>{" "}
+          <span className="text-lime-300 font-bold">
+            , Tailwind CSS and Shadcn UI for styling
+          </span>
+          , and <span className="text-lime-300 font-bold">Redux Toolkit</span>{" "}
+          for state management.{" "}
+          <span className="text-lime-300 font-bold">
+            React Query handles efficient data fetching
+          </span>{" "}
+          , while <span className="text-lime-300 font-bold">MongoDB</span>{" "}
+          stores product, user, and order data. Payments are processed securely
+          via <span className="text-lime-300 font-bold">Stripe</span>, and{" "}
+          <span className="text-lime-300 font-bold">AWS S3</span> is used for
+          image storage. The site includes full{" "}
+          <span className="text-lime-300 font-bold">
+            CRUD operations through REST APIs
+          </span>
+          , enabling <span className="text-lime-300 font-bold">seamless</span>{" "}
+          product management, order processing, and user authentication.
+        </p>
+      </div>
+    </div>
+  );
 }
